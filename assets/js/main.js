2d10a8ec208e74dc00c442de8e4fc9aea844115c
gia fauzan <giafauzan11@gmail.com>
2023-03-09 20:23:54 +0700

    responsive

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".portofolioSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+var swiper2 = new Swiper(".client-response", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev"
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
